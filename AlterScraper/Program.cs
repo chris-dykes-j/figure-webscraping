578a4661621da19ea1b6950ccd9683a5d2036bdc
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-03-06 10:44:55 -0500

    Update program to iterate through given years.

--- AlterScraper/Program.cs
+++ AlterScraper/Program.cs
@@ -1,12 +1,14 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
-Console.Write("Input year: ");
+Console.Write("Input first year: ");
 var searchYear = Console.ReadLine();
+Console.Write("Input last year: ");
+var finalYear = Console.ReadLine();
 Console.Write("Input section: ");
 var section = Console.ReadLine();
-const string brand = "Alter";
 
 const string csvPath = "/home/chris/RiderProjects/FigureWebScraper/AlterScraper/alter-jp.csv";
 if (!File.Exists(csvPath))
@@ -26,63 +28,80 @@ httpClient.DefaultRequestHeaders.UserAgent.Add(system);
 httpClient.DefaultRequestHeaders.UserAgent.Add(gecko);
 httpClient.DefaultRequestHeaders.UserAgent.Add(firefox);
 
-var requestUri = $"https://alter-web.jp/{section}/?yy={searchYear}&mm=";
-using var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
-
-var response = await httpClient.SendAsync(request).Result.Content.ReadAsStringAsync();
-var homePage = new HtmlDocument();
-homePage.LoadHtml(response);
-
-var random = new Random();
-
-foreach (var link in homePage.DocumentNode.SelectNodes("//figure/a"))
+for (var year = int.Parse(searchYear!); year >= int.Parse(finalYear!); year--)
 {
-    Thread.Sleep(random.Next(1000, 3000));
-    var figureLink = "https://alter-web.jp" + link.Attributes["href"].Value;
-    using var figureRequest = new HttpRequestMessage(HttpMethod.Get, figureLink);
-    figureRequest.Headers.Referrer = new Uri(requestUri);
-    var figureResponse = await httpClient.SendAsync(figureRequest).Result.Content.ReadAsStringAsync();
-    var figurePage = new HtmlDocument();
-    figurePage.LoadHtml(figureResponse);
-    var result = await ParseFigurePage(figurePage, figureLink);
-    File.AppendAllText(csvPath, result + "\n");
-    Console.WriteLine(result);
+    await GetFigureData(year);
+    Thread.Sleep(15000);
+}
+
+async Task GetFigureData(int year)
+{
+    var requestUri = $"https://alter-web.jp/{section}/?yy={year}&mm=";
+    using var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+    var response = await httpClient.SendAsync(request).Result.Content.ReadAsStringAsync();
+    var homePage = new HtmlDocument();
+    homePage.LoadHtml(response);
+    
+    var random = new Random();
+    foreach (var link in homePage.DocumentNode.SelectNodes("//figure/a"))
+    {
+        Thread.Sleep(random.Next(1000, 3000));
+        var figureLink = "https://alter-web.jp" + link.Attributes["href"].Value;
+        using var figureRequest = new HttpRequestMessage(HttpMethod.Get, figureLink);
+        figureRequest.Headers.Referrer = new Uri(requestUri);
+        var figureResponse = await httpClient.SendAsync(figureRequest).Result.Content.ReadAsStringAsync();
+        var figurePage = new HtmlDocument();
+        figurePage.LoadHtml(figureResponse);
+        var result = await ParseFigurePage(figurePage, figureLink, year);
+        File.AppendAllText(csvPath, result + "\n");
+        Console.WriteLine(result);
+    }
 }
 
 Console.WriteLine("Scraping complete!");
 
-async Task<string> ParseFigurePage(HtmlDocument figurePage, string figureLink)
+async Task<string> ParseFigurePage(HtmlDocument figurePage, string figureLink, int year)
 {
     var name = figurePage.DocumentNode.SelectSingleNode($"//h1[@class='hl06 c-{section}']");
     var tableItems = figurePage.DocumentNode.SelectNodes("//td");
     var material = figurePage.DocumentNode.SelectSingleNode("//span[@class='txt']");
     var blogLinks = figurePage.DocumentNode.SelectNodes("//div[@class='imgtxt imgtxt-type-b img-l']//a");
+    const string brand = "Alter";
     
     var result = $"\"{name.InnerText}\","; 
     foreach (var item in tableItems)
     {
-        result += $"\"{item.InnerHtml}\",".Replace("<br>", " ");
+        result += $"\"{item.InnerHtml}\",".Replace("<br>", " ")
+            .Replace("<span class=\"resale c-figure\">再販</span>", " ");
     }
     result += $"\"{material.InnerText}\",{brand},{figureLink},";
 
     result += "\"";
-    foreach (var blogLink in blogLinks)
+    if (blogLinks != null)
     {
-        result += $"{blogLink.Attributes["href"].Value},";
+        foreach (var blogLink in blogLinks)
+        {
+            result += $"{blogLink.Attributes["href"].Value},";
+        }
     }
     result += "\"";
     
-    await GetFigureImages(figurePage, name.InnerText, figureLink);
+    await GetFigureImages(figurePage, name.InnerText, figureLink, year);
     
     return Regex.Replace(result, @"\p{Cc}", "");
 }
 
-async Task GetFigureImages(HtmlDocument figurePage, string name, string referrer)
+async Task GetFigureImages(HtmlDocument figurePage, string name, string referrer, int year)
 {
-    var path = $"/run/media/chris/Extra/Figures/Alter/{searchYear}/{name}";
+    var path = $"/run/media/chris/Extra/Figures/Alter/{year}/{name}";
     if (!Directory.Exists(path))
     {
         Directory.CreateDirectory(path);
+    }
+    else
+    {
+        return;
     }
     
     var firstImage = figurePage.DocumentNode.SelectSingleNode("//div[@class='item-mainimg']//img");
@@ -109,13 +128,16 @@ async Task GetFigureImages(HtmlDocument figurePage, string name, string referrer
     }
 
     var iconLinks = figurePage.DocumentNode.SelectNodes("//div[@class='imgtxt imgtxt-type-b img-l']//img");
-    foreach (var iconLink in iconLinks)
+    if (iconLinks != null)
     {
-        var iconPath = iconLink.Attributes["src"].Value;
-        using var iconRequest = new HttpRequestMessage(HttpMethod.Get, $"https://alter-web.jp{iconPath}");
-        iconRequest.Headers.Referrer = new Uri(referrer);
-        await using var streamToRead = await httpClient.SendAsync(iconRequest).Result.Content.ReadAsStreamAsync();
-        await using var streamToWrite = File.Open($"{path}/{iconPath[20..]}", FileMode.Create);
-        await streamToRead.CopyToAsync(streamToWrite);
+        foreach (var iconLink in iconLinks)
+        {
+            var iconPath = iconLink.Attributes["src"].Value;
+            using var iconRequest = new HttpRequestMessage(HttpMethod.Get, $"https://alter-web.jp{iconPath}");
+            iconRequest.Headers.Referrer = new Uri(referrer);
+            await using var streamToRead = await httpClient.SendAsync(iconRequest).Result.Content.ReadAsStreamAsync();
+            await using var streamToWrite = File.Open($"{path}/{iconPath[20..]}", FileMode.Create);
+            await streamToRead.CopyToAsync(streamToWrite);
+        }
     }
 }

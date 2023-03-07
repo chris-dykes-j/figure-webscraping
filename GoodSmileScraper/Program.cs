0eecaff482de27042a0096243867b14e30f87b68
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-03-07 11:32:41 -0500

    Begin GoodSmile project. Scrape more data and fix exceptions. Begin ScraperTools project.

--- GoodSmileScraper/Program.cs
+++ GoodSmileScraper/Program.cs
@@ -1,31 +1,14 @@
-﻿// Input which year/years to scrape.
+﻿using ScraperTools;
 
-// Create new csv file.
+const string csvPath = "/home/chris/RiderProjects/FigureWebScraper/GoodSmileScraper/goodsmile-table.csv";
+if (!File.Exists(csvPath))
+{
+    await using var writer = File.CreateText(csvPath);
+    writer.WriteLine("product-name,series,manufacturer,category,price,release,specifications,sculptor,paintwork,planning/production," +
+                     "designer,illustrator,released-by,distributed-by,cooperation,director,url");
+}
 
-// Create header to mimic regular user. Great way to learn about request headers.
+var httpClient = new ScraperClient().HttpClient;
 
-using System.Net.Http.Headers;
-
-var httpClient = new HttpClient();
-var userAgent = new ProductInfoHeaderValue("Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0");
-httpClient.DefaultRequestHeaders.UserAgent.Add(userAgent);
-
-var request = new HttpRequestMessage(HttpMethod.Get, "https:www.google.ca");
-var response = await httpClient.SendAsync(request);
-
-var test = response;
-
-// Make list of links. <a> tags with /products/ href.
-
-// Make timed requests to each link, and get data.
-
-// Add each line to a csv file.
-
-// Save images to a separate directories?
-
-// ?
-
-// Profit.
-
-Console.WriteLine("Profit");
+Console.WriteLine("Scraping complete!");
 
---
+++ ScraperTools/ScraperClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+namespace ScraperTools;
+
+public class ScraperClient
+{
+    public HttpClient HttpClient { get; } 
+    private readonly ProductInfoHeaderValue _mozilla = new("Mozilla", "5.0");
+    private readonly ProductInfoHeaderValue _system = new("(X11; Linux x86_64; rv:108.0)");
+    private readonly ProductInfoHeaderValue _gecko = new("Gecko", "20100101");
+    private readonly ProductInfoHeaderValue _firefox = new("FireFox", "108.0");
+    
+    public ScraperClient()
+    {
+        HttpClient = new HttpClient();
+        HttpClient.DefaultRequestHeaders.UserAgent.Add(_mozilla);
+        HttpClient.DefaultRequestHeaders.UserAgent.Add(_system);
+        HttpClient.DefaultRequestHeaders.UserAgent.Add(_gecko);
+        HttpClient.DefaultRequestHeaders.UserAgent.Add(_firefox);
+    }
+}

d6e8a5f4977206084cd98c4f89c7fb2ca93b21d4
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-03-30 14:23:03 -0400

    Initialize AlterNormalization project.

---
+++ AlterNormalization/Program.cs
@@ -0,0 +1,9 @@
+// Read the file.
+
+// Handle repeating groups in release, price, size, sculptor, painter, material, blog_url
+
+// Separate year and month.
+
+// Separate scale from size.
+
+Console.WriteLine("cool fam");

0297d0816918afe1034aac91f7c23aa068f2e137
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-03-31 14:45:25 -0400

    Fix instances where scale does not exist.

--- AlterNormalization/Program.cs
+++ AlterNormalization/Program.cs
@@ -3,21 +3,29 @@ using System.Text.RegularExpressions;
 const string readPath = "/home/chris/RiderProjects/FigureWebScraper/AlterScraper/alter-jp.csv";
 
 string CsvPath(string tableName) => 
-    $"/home/chris/RiderProjects/FigureWebScraper/AlterNormalization/alter-{tableName}-jp.csv";
+    $"/home/chris/RiderProjects/FigureWebScraper/AlterNormalization/alter-{tableName}-jp-test.csv";
 
 using var streamWriter = File.CreateText(CsvPath("figures"));
 using var streamReader = new StreamReader(readPath);
 
 Console.WriteLine("Starting normalization");
+var isFirstLine = true;
 while (!streamReader.EndOfStream)
 {
     var line = streamReader.ReadLine();
+    if (isFirstLine)
+    {
+        File.AppendAllText(CsvPath("figures"), "name,series,character,scale,brand,origin_url\n");
+        isFirstLine = false;
+        continue;
+    }
     var columns = SplitIgnoringQuotes(line!, ',');
     var scaleRegex = new Regex(@"^1\/[1-9]");
-    var scale = scaleRegex.Match(columns[5]).ToString();
-    if (string.IsNullOrEmpty(scale)) scale = "scale";
-    //var size = scaleRegex.Replace(columns[5], "").Replace("スケール ", "");
-    var figuresValues = $"{columns[0]},{columns[1]},{columns[2]},{scale},{columns[9]},{columns[10]}";
+    var figureScale = scaleRegex.Match(columns[5]).ToString();
+    //var size = scaleRegex.Replace(columns[5], "").Replace("スケール", "");
+    
+    // Figures table
+    var figuresValues = $"{columns[0]},{columns[1]},{columns[2]},{figureScale},{columns[9]},{columns[10]}";
     File.AppendAllText(CsvPath("figures"), figuresValues + '\n');
     Console.WriteLine(figuresValues);
 }

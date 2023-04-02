f333e0d9e923c32f976b0d535da344eddc334a8d
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-04-02 12:29:45 -0400

    Create painters csv

--- AlterNormalization/Processors/PaintersCsvProcessor.cs
+++ AlterNormalization/Processors/PaintersCsvProcessor.cs
@@ -1,16 +1,48 @@
+using System.Text.RegularExpressions;
+
 namespace AlterNormalization.Processors;
 
 public class PaintersCsvProcessor : CsvProcessor
 {
     public PaintersCsvProcessor(string outPath) : base(outPath) { }
 
-    public override string ProcessFirstLine()
-    {
-        throw new NotImplementedException();
-    }
+    public override string ProcessFirstLine() => "name,painter\n";
 
     public override string ProcessLine(string line)
     {
-        throw new NotImplementedException();
+        var columns = SplitIgnoringQuotes(line, ',');
+        var splitPaintersFirst = SplitSculptorsByBracket(Regex.Split(columns[6], @"[】）]\s"));
+        var splitPaintersSecond = SplitPaintersByPlus( splitPaintersFirst);
+
+        var result = "";
+        foreach (var painter in splitPaintersSecond)
+        {
+            result += $"{columns[0]},{painter}\n";
+        }
+
+        return result;
     }
+    
+    private List<string> SplitSculptorsByBracket(string[] painters)
+    {
+        var result = new List<string>();
+        foreach (var painter in painters)
+        {
+            var sculptorResult = painter;
+            if (string.IsNullOrWhiteSpace(painter)) continue;
+            if (painter.Contains("原型協力：アルター")) continue;
+            if (MissingSquareBracket(painter)) sculptorResult += '】';
+            if (MissingParenthesis(painter)) sculptorResult += '）';
+            result.Add(sculptorResult);
+        }
+
+        return result;
+    }
+
+    private List<string> SplitPaintersByPlus(List<string> painters) => 
+        painters.SelectMany(sculptor => sculptor.Split('＋', StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+    private bool MissingSquareBracket(string input) => input.Contains('【') && !input.Contains('】');
+    
+    private bool MissingParenthesis(string input) => input.Contains('（') && !input.Contains('）');
 }

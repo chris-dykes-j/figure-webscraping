3c45cbc2cea1bee0816f8b78fdef349e232dd678
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-04-07 10:26:01 -0400

    Finish characters csv normalization.

--- AlterNormalization/Processors/CharactersCsvProcessor.cs
+++ AlterNormalization/Processors/CharactersCsvProcessor.cs
@@ -4,27 +4,49 @@ public class CharactersCsvProcessor : CsvProcessor
 {
     public CharactersCsvProcessor(string outPath) : base(outPath) { }
 
-    public override string ProcessFirstLine() => "name,character";
+    public override string ProcessFirstLine() => "name,character\n";
 
+    private int i = 0;
     public override string ProcessLine(string line)
     {
         var columns = SplitIgnoringQuotes(line, ',');
-        var charactersColumn = SplitCharacters(columns);
+        var charactersColumn = GetCharacters(columns);
         
-        return charactersColumn.Aggregate("", (current, character) =>
-        {
-            if (IsRyzaCharacter(character))
-                return current.TrimEnd('\n').Trim() + $"{character}\n";
-            return current + $"{columns[0]},{character}\n";
-        });
+        return charactersColumn.Aggregate("", (current, character) => //IsTwoCharacters(character) 
+            $"{current}{columns[0]},{character.TrimStart()}\n");
     }
 
-    private IEnumerable<string> SplitCharacters(List<string> columns)
+    private IEnumerable<string> GetCharacters(List<string> columns)
     {
+        if (IsRyzaCharacter(columns[2])) return GetRyzaCharacter(columns[2]);
+        if (IsEdgeCaseCharacter(columns[0], columns[2])) return GetEdgeCaseCharacters(columns[0], columns[2]);
         return string.IsNullOrWhiteSpace(columns[2])
-            ? columns[0].Split('&', StringSplitOptions.RemoveEmptyEntries)
-            : columns[2].Split('&', StringSplitOptions.RemoveEmptyEntries);
+            ? columns[0].Split(new[]{'&','＆'}, StringSplitOptions.RemoveEmptyEntries)
+            : columns[2].Split(new[]{'&', '＆'}, StringSplitOptions.RemoveEmptyEntries);
     }
 
-    private bool IsRyzaCharacter(string character) => character.Contains("the Secret Hideout");
+    private bool IsRyzaCharacter(string character) => character.Contains("Atelier Ryza: Ever Darkness & the Secret Hideout");
+    private bool IsEdgeCaseCharacter(string nameColumn, string characterColumn)
+    {
+        var character = string.IsNullOrWhiteSpace(characterColumn) ? nameColumn : characterColumn;
+        return character.Contains("AZUR LANE IJN Atago & IJN Takao") ||
+               character.Contains("ALVIS & LAVIE BY LASTEXILE") ||
+               character.Contains("シャマル & ザフィーラ 仔犬Ver.") ||
+               character.Contains("ユーリ・ローウェル　心の中の聖騎士様Ver.＆ラピード");
+    }
+
+    private IEnumerable<string> GetRyzaCharacter(string characters) => new[] { characters };
+
+    private IEnumerable<string> GetEdgeCaseCharacters(string nameColumn, string characterColumn)
+    {
+        var characters = string.IsNullOrWhiteSpace(characterColumn) ? nameColumn : characterColumn;
+        return characters switch
+        {
+            _ when characters.Contains("AZUR LANE IJN Atago & IJN Takao") => new [] { "AZUR LANE IJN Atago", "AZUR LANE IJN Takao" },
+            _ when characters.Contains("ALVIS & LAVIE BY LASTEXILE") => new [] { "ALVIS BY LASTEXILE", "LAVIE BY LASTEXILE" },
+            _ when characters.Contains("シャマル & ザフィーラ 仔犬Ver.") => new [] {"シャマル", "ザフィーラ" },
+            _ when characters.Contains("ユーリ・ローウェル　心の中の聖騎士様Ver.＆ラピード") => new []{ "ユーリ・ローウェル", "ラピード"},
+            _ => new [] {""}
+        };
+    }
 }
--- AlterNormalization/Processors/FiguresCsvProcessor.cs
+++ AlterNormalization/Processors/FiguresCsvProcessor.cs
@@ -6,7 +6,7 @@ public class FiguresCsvProcessor : CsvProcessor
 {
     public FiguresCsvProcessor(string outPath) : base(outPath) { }
 
-    public override string ProcessFirstLine() => "name,series,character,scale,brand,origin_url\n";
+    public override string ProcessFirstLine() => "name,series,scale,brand,origin_url\n";
 
     public override string ProcessLine(string line)
     {
@@ -14,6 +14,6 @@ public class FiguresCsvProcessor : CsvProcessor
         var scaleRegex = new Regex(@"^1/\d+");
         var figureScale = scaleRegex.Match(columns[5]).ToString();
 
-        return $"{columns[0]},{columns[1]},{columns[2]},{figureScale},{columns[9]},{columns[10]}\n";
+        return $"{columns[0]},{columns[1]},{figureScale},{columns[9]},{columns[10]}\n";
     }
 }

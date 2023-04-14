70ddc803d6fc595a57c9aa528554d40de5e9894a
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-04-14 14:47:23 -0400

    Update select.sql.

--- AlterSql/select.sql
+++ AlterSql/select.sql
@@ -2,7 +2,11 @@ SELECT
   f.*,
   fn.text AS figure_name,
   sn.text AS series_name,
-  cn.text AS character_name
+  cn.text AS character_name,
+  bu.blog_url AS blog_url,
+  p.price_with_tax,
+  p.price_without_tax,
+  p.currency
 FROM
   public.figure AS f
     LEFT JOIN
@@ -10,4 +14,8 @@ FROM
     LEFT JOIN
   public.series_name AS sn ON f.id = sn.figure_id
     LEFT JOIN
-  public.character_name AS cn ON f.id = cn.figure_id;
+  public.character_name AS cn ON f.id = cn.figure_id
+    LEFT JOIN
+  public.blog_url AS bu on f.id = bu.figure_id
+    LEFT JOIN
+  public.price AS p on f.id = p.figure_id;
